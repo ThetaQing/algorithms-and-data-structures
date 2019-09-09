3f53829091889b42c71bc5d5130f03f0e44626c2
ThetaQing <csusq2016@163.com>
2019-09-09 21:00:50 +0800

    Update palindromeNumber.cpp
    更新算法，时间16ms，内存8.1m

--- Project1/palindromeNumber.cpp
+++ Project1/palindromeNumber.cpp
@@ -1,3 +1,30 @@
+// 时间16ms，内存8.1m
+// 主要思想是存储在另一个变量中，返回这两个数是否相等即可
+class Solution {
+public:
+    bool isPalindrome(int x) {       
+        
+       int temp=0;
+        long int y=0;  // 注意这里的长度，如果只是int会导致溢出
+        int input_x = x;
+        if(x<0)
+            return false;
+        else if(x==0)
+            return true;
+        
+        while(x)
+        {
+            temp = x%10;            
+            x = x/10;
+            y = y*10 + temp;
+        }
+        return y==input_x;
+        
+    }
+};
+
+//  花费时间44ms
+// 主要思想是一位一位地比较
 class Solution {
 public:
     bool isPalindrome(int x) {
@@ -24,4 +51,4 @@ public:
         
     }
 };
-//  花费时间44ms
+
