c9d778f0ac124d9003922eb3a3c4e3f353205e6d
ThetaQing <csusq2016@163.com>
2019-09-19 21:33:15 +0800

    Qsort()

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -10,17 +10,40 @@
 # include <typeinfo>
 # include ".\LeetCode\LeetCode.h"
 #include <algorithm>
+#include <stdio.h>
+#include "mysort.h"
 
 using std::vector;
 using namespace std;
 
 using std::string;
+#define N 7
+int fun1(char s[], char a, int n)
+{
+	int j;
+	j = n;
+	while (a < s[j] && j > 0)
+		j--;
+	return j;
+}
+int fun2(char s[], char a, int n)
+{
+	int j;
+	j = 1;
+	while (a > s[j] && j <= n)
+		j++;
+	return j;
+}
+
 
 int main()
 {
-	TestBiTree();
-
-	
+	int a[] = { 57, 68, 59, 52, 72, 28, 96, 33, 24 };
+	int b = 0;
+	int* p = &b;
+	cout << *(a+1) << endl;
+	cout << sizeof(a) << endl;
+	return 0;
 	system("pause");
-
 }
+
---
+++ Project1/mysort.h
@@ -0,0 +1,4 @@
+#ifndef __MYSORT_H__
+#define __MYSORT_H__
+void Qsort(int arr[], int low, int high);
+#endif // !__MYSORT_H__
