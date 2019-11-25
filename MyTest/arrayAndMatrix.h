63df7fc9a4823cc8171f5384d1396142d999c4cf
ThetaQing <csusq2016@163.com>
2019-11-25 22:24:41 +0800

    三对角矩阵和下三角矩阵的实现

--- MyTest/arrayAndMatrix.h
+++ MyTest/arrayAndMatrix.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <sstream>
+#include "myExceptions.h"
 using namespace std;
 /*****************文件描述********************
 * 文件名：arrayAndMatrix.h
@@ -11,19 +12,27 @@ using namespace std;
 
 **/
 
-
+#ifndef __MATRIX_
+#define __MATRIX_
 /********************程序说明*******************
 =================================================
 
 * 程序编号：7-2  P153
-* 程序功能：矩阵类matrix的声明
+* 程序功能：矩阵类matrix的声明与实现
+* 类的成员函数：
+			1、构造函数（默认构造函数和复制构造函数）；
+			2、析构函数；
+			3、返回行数的函数；
+			4、返回列数的函数；
+			5、重载运算符=、+、-、（）、*
+			6、转置运算
 
 
 =================================================
 **/
 
 /****************程序说明***********
-* 程序功能：matrix类的说明
+* 程序功能：matrix类的声明
 
 
 */
@@ -35,17 +44,17 @@ public:
 	matrix(int theRows = 0, int theColumns = 0);
 	matrix(const matrix<T>&);
 	~matrix() { delete[] element; }
-	int rows() const { return theRows; };
-	int columns() const { return theColumns; }
+	int rows() const { return theRows; };  // 返回行数的函数
+	int columns() const { return theColumns; }  // 返回列数的函数
 	T& operator() (int i, int j) const;
 	matrix<T>& operator = (const matrix<T>&);
 	matrix<T> operator +() const;  // 单目运算符+
 	matrix<T> operator +(const matrix<T>&) const;
 	matrix<T> operator -() const;  // 单目运算符-
 	matrix<T> operator -(const matrix<T>&) const;
-	matrix<T>& operator +=(const T&);  
+	matrix<T>& operator +=(const T&);
 	matrix<T> operator *(const matrix<T>&) const;
-	
+
 	// 转置运算
 	matrix<T> trans();
 
@@ -250,18 +259,267 @@ matrix<T> matrix<T>::trans()
 	{
 		for (int j = 1; j <= theColumns; ++j)
 		{
-			
+
 			m(j, i) = n(i, j);
 		}
-		
+
 	}
 	return m;
 }
 
+#endif // !__MATRIX_
+
+
 // 其他函数声明
 void testMain();
 
+
+
+#ifndef __DIAGONALMATRIX_
+#define __DIAGONALMATRIX_
+
+/****************对角矩阵******************
+* 类名：diagonalMatrix<T>
+* 说明：一个row*rows的对角矩阵D，最多只有rows个非0元素，因此可以用一个一维数组element[rows]来表示对角矩阵，其中，
+		element[i-1]表示D(i,i)，所有未在一维数组中出现的矩阵元素均为0.
+* 时间复杂度：对于构造函数的时间复杂度来说，当T是内部类型时为O(1)，当T是用户定义类型时为O(rows)；
+			  对于get方法和set方法的时间复杂度都是O(1)。
+
+**/
+
+template<class T>
+class diagonalMatrix
+{
+public:
+	diagonalMatrix(int theN = 10);
+	~diagonalMatrix() { delete[] element; }
+	T get(int, int) const;
+	void set(int, int, const T&);
+
+private:
+	int n;  // 矩阵维数；
+	T* element;  // 存储对角矩阵的一维数组
+};
+
+// 构造函数
+template <class T>
+diagonalMatrix<T>::diagonalMatrix(int theN)
+{
+	// 检验theN的值是否有效
+	if (theN < 1)
+		throw "Matrix size must be > 0";
+	n = theN;
+	element = new T[n];
+}
+
+// get方法
+template <class T>
+T diagonalMatrix<T>::get(int i, int j)const
+{
+	//检验参数是否有效
+	if (i < 1 || j < 1 || i > n || j > n)
+		throw "matrix index out of bounds";
+	else if (i == j)
+		return element[i - 1];  // 返回对角线上的元素
+	else
+		return 0;  // 非对角线上的元素
+}
+
+// set方法
+template<class  T>
+void diagonalMatrix<T>::set(int i, int j, const T& x)
+{
+	//检验参数是否有效
+	if (i < 1 || j < 1 || i > n || j > n)
+		throw "matrix index out of bounds";
+	else if (i == j)
+		element[i - 1] = x;
+	else
+		if (x != 0)
+			throw "nondiagonal element must be zero";
+}
+
+
+
+
+#endif // !__DIAGONALMATRIX_
+
+
+
+#ifndef __TRIDIAGONALMATRIX_
+#define __TRIDIAGONALMATRIX_
+
+
+/****************三对角矩阵******************
+* 类名：tridiagonalMatrix<T>
+* 说明：在一个rows*rows的三对角矩阵中，非0元素排列在如下三条对角线上：
+			1、主对角线：`i = j`;
+			2、主对角线之下的对角线（称为低对角线）：`i = j + 1`;
+			3、主对角线之上的对角线（称为高对角线）：`i = j - 1`;
+		这三条对角线的元素总数为`3*rows-2`，可以用一个容量为`3*rows-2`的一维数组element来描述三对角矩阵；一共有三种映射关系：
+			1、逐行映射；
+			2、逐列映射；
+			3、从最下面那条对角线开始逐条对角线映射。
+* 时间复杂度：对于构造函数的时间复杂度来说，当T是内部类型时为O(1)，当T是用户定义类型时为O(rows)；
+			  对于get方法和set方法的时间复杂度都是O(1)。
+
+**/
+
+
+
+
+
+
+template<class T>
+class tridiagonalMatrix
+{
+public:
+	tridiagonalMatrix(int theN = 10);
+	~tridiagonalMatrix() { delete[] element; }
+	T get(int, int) const;
+	void set(int, int, const T&);
+private:
+	int n;       // matrix dimension
+	T* element;  // 1D array for tridiagonal
+};
+
+template<class T>
+tridiagonalMatrix<T>::tridiagonalMatrix(int theN)
+{// Constructor.
+   // validate theN
+	if (theN < 1)
+		throw illegalParameterValue("Matrix size must be > 0");
+
+	n = theN;
+	element = new T[3 * n - 2];
+}
+
+template <class T>
+T tridiagonalMatrix<T>::get(int i, int j) const
+{// Return (i,j)th element of matrix.
+
+   // validate i and j
+	if (i < 1 || j < 1 || i > n || j > n)
+		throw matrixIndexOutOfBounds();
+
+	// determine lement to return
+	switch (i - j)
+	{
+	case 1: // lower diagonal
+		return element[i - 2];
+	case 0: // main diagonal
+		return element[n + i - 2];
+	case -1: // upper diagonal
+		return element[2 * n + i - 2];
+	default: return 0;
+	}
+}
+
+template<class T>
+void tridiagonalMatrix<T>::set(int i, int j, const T& newValue)
+{// Store newValue as (i,j)th element
+
+   // validate i and j
+	if (i < 1 || j < 1 || i > n || j > n)
+		throw matrixIndexOutOfBounds();
+
+	switch (i - j)
+	{
+	case 1: // lower diagonal
+		element[i - 2] = newValue; break;
+	case 0: // main diagonal
+		element[n + i - 2] = newValue; break;
+	case -1: // upper diagonal
+		element[2 * n + i - 2] = newValue; break;
+	default: if (newValue != 0)
+		throw illegalParameterValue
+		("non-tridiagonal elements must be zero");
+	}
+}
+
+#endif // !__TRIDIAGONALMATRIX_
+
+#ifndef __LOWERTRAIANGULARMATRIX_
+#define __LOWERTRAIANGULARMATRIX_
+/**************下三角矩阵**************
+* 类名：lowerTriangularMatrix<T>
+* 类的说明：
+
+
+
+**/
+
+template<class T>
+class lowerTriangularMatrix
+{
+public:
+	lowerTriangularMatrix(int theN = 10);
+	~lowerTriangularMatrix() { delete[] element; }
+	T get(int, int) const;
+	void set(int, int, const T&);
+private:
+	int n;       // 矩阵维数
+	T* element;  // 一维数组
+};
+
+
+template<class T>
+lowerTriangularMatrix<T>::lowerTriangularMatrix(int theN)
+{// Constructor.
+   // validate theN
+	if (theN < 1)
+		throw illegalParameterValue("Matrix size must be > 0");
+
+	n = theN;
+	element = new T[n * (n + 1) / 2];
+}
+
+template <class T>
+T lowerTriangularMatrix<T>::get(int i, int j) const
+{// Return (i,j)th element of matrix.
+   // validate i and j
+	if (i < 1 || j < 1 || i > n || j > n)
+		throw matrixIndexOutOfBounds();
+
+	// (i,j) in lower triangle iff i >= j
+	if (i >= j)
+		return element[i * (i - 1) / 2 + j - 1];
+	else
+		return 0;
+}
+
+template<class T>
+void lowerTriangularMatrix<T>::set(int i, int j, const T& newValue)
+{// Store newValue as (i,j)th element.
+   // validate i and j
+	if (i < 1 || j < 1 || i > n || j > n)
+		throw matrixIndexOutOfBounds();
+
+
+	// (i,j) in lower triangle iff i >= j
+	if (i >= j)
+		element[i * (i - 1) / 2 + j - 1] = newValue;
+	else
+		if (newValue != 0)
+			throw illegalParameterValue
+			("elements not in lower triangle must be zero");
+}
+
+#endif // !__LOWERTRAIANGULARMATRIX_
+
+#ifndef __SPAREMATRIX_
+#define __SPAREMATRIX_
+
+/****************稀疏矩阵***********
+* 类名：
+* 类的说明：一个m*n的矩阵，如果大多数元素都是0，则称为稀疏矩阵，一个矩阵如果不是稀疏的，就称为稠密矩阵。该部分着重分析非0区域不规则的稀疏矩阵。
+* 
+
+
+**/
+
+
+#endif // !__SPAREMATRIX_
+
+
 #endif
-
-
-
